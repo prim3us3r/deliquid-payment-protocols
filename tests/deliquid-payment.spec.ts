0c2af3c37f35c099fbcc226ea13d3de5733283f5
Kailing Ding <markdingkl@gmail.com>
2022-04-06 00:52:08 -0700

    Update deliquid-payment.spec.ts

--- tests/deliquid-payment.spec.ts
+++ tests/deliquid-payment.spec.ts
@@ -1,30 +1,35 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { DeliquidPayment } from '../target/types/deliquid_payment';
-import { Keypair } from '@solana/web3.js';
+import { Keypair, SystemProgram } from '@solana/web3.js';
 
 describe('deliquid-payment', () => {
   anchor.setProvider(anchor.Provider.env());
 
+  const connection = anchor.getProvider().connection;
   const program = anchor.workspace.DeliquidPayment as Program<DeliquidPayment>;
+  const merchant = Keypair.generate();
   const merchantOwner = Keypair.generate();
   const customer = Keypair.generate();
 
   it('Create a merchant account!', async () => {
-    const tx = await program.methods
+    const signature = await program.methods
       .createMerchant('First Merchant')
       .accounts({
+        merchant: merchant.publicKey,
         owner: merchantOwner.publicKey,
       })
-      .signers([merchantOwner])
+      .signers([merchant])
       .rpc();
-    console.log(tx);
+
+    console.log(signature);
   });
 
   it('Update a merchant account!', async () => {
     const tx = await program.methods
       .updateMerchant('Merchant name updated')
       .accounts({
+        merchant: merchant.publicKey,
         owner: merchantOwner.publicKey,
       })
       .signers([merchantOwner])
@@ -33,11 +38,15 @@ describe('deliquid-payment', () => {
   });
 
   it('Create a charge by a customer', async () => {
+    const charge = Keypair.generate();
+
     const tx = await program.methods
       .createCharge('First Charge')
       .accounts({
+        charge: charge.publicKey,
         customer: customer.publicKey,
         merchant: merchantOwner.publicKey,
+        systemProgram: SystemProgram.programId,
       })
       .signers([customer])
       .rpc();
