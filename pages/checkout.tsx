28136ba13f71efdcb79c78f60815bf3b28df6e37
engkudigital <engkudigital@gmail.com>
2022-04-06 20:53:37 +0800

    cekkk

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,125 +1,72 @@
-import {
-  findTransactionSignature,
-  FindTransactionSignatureError,
-  createQR,
-  encodeURL,
-  EncodeURLComponents,
-} from '@solana/pay'
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { Keypair, Transaction, clusterApiUrl, PublicKey } from '@solana/web3.js'
-import { useRouter } from 'next/router'
-import { useEffect, useMemo, useState, useRef } from 'react'
-import BackLink from '../components/BackLink'
-import Loading from '../components/Loading'
-import {
-  MakeTransactionInputData,
-  MakeTransactionOutputData,
-} from './api/makeTransaction'
-import { shopAddress, usdcAddress } from './../lib/addresses'
-import calculatePrice from './../lib/calculatePrice'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { findTransactionSignature, FindTransactionSignatureError } from "@solana/pay";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Keypair, Transaction } from "@solana/web3.js";
+import { useRouter } from "next/router";
+import { useEffect, useMemo, useState } from "react";
+import BackLink from "../components/BackLink";
+import Loading from "../components/Loading";
+import { MakeTransactionInputData, MakeTransactionOutputData } from "./api/makeTransaction";
 
-export const config = {
-  api: {
-    bodyParser: false,
-    externalResolver: true,
-  },
-}
 export default function Checkout() {
-  const router = useRouter()
-  const { connection } = useConnection()
-  const { publicKey, sendTransaction } = useWallet()
-
-  // ref to a div where we'll show the QR code
-  const qrRef = useRef<HTMLDivElement>(null)
-  const amount = useMemo(() => calculatePrice(router.query), [router.query])
-
-  // Get a connection to Solana devnet
-  const network = WalletAdapterNetwork.Devnet
-  const endpoint = clusterApiUrl(network)
-
-  // Show the QR code
-  useEffect(() => {
-    const qr = createQR(url, 512, 'transparent')
-    if (qrRef.current && amount.isGreaterThan(0)) {
-      qrRef.current.innerHTML = ''
-      qr.append(qrRef.current)
-    }
-  })
+  const router = useRouter();
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
 
   // State to hold API response fields
-  const [transaction, setTransaction] = useState<Transaction | null>(null)
-  const [message, setMessage] = useState<string | null>(null)
+  const [transaction, setTransaction] = useState<Transaction | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
 
   // Read the URL query (which includes our chosen products)
-  const searchParams = new URLSearchParams()
+  const searchParams = new URLSearchParams();
   for (const [key, value] of Object.entries(router.query)) {
     if (value) {
       if (Array.isArray(value)) {
         for (const v of value) {
-          searchParams.append(key, v)
+          searchParams.append(key, v);
         }
       } else {
-        searchParams.append(key, value)
+        searchParams.append(key, value);
       }
     }
   }
 
   // Generate the unique reference which will be used for this transaction
-  const reference = useMemo(() => Keypair.generate().publicKey, [])
+  const reference = useMemo(() => Keypair.generate().publicKey, []);
 
-  // Solana Pay transfer params
-  const urlParams: EncodeURLComponents = {
-    recipient: shopAddress,
-    splToken: usdcAddress,
-    amount,
-    reference,
-    label: 'Cupcake Coding Co',
-    message: 'Thanks for your order! 🧁',
-  }
   // Add it to the params we'll pass to the API
-  searchParams.append('reference', reference.toString())
-
-  // Encode the params into the format shown
-  const url = encodeURL(urlParams)
-  console.log({ url })
+  searchParams.append('reference', reference.toString());
 
   // Use our API to fetch the transaction for the selected items
   async function getTransaction() {
     if (!publicKey) {
-      return
+      return;
     }
 
     const body: MakeTransactionInputData = {
       account: publicKey.toString(),
     }
 
-    const response = await fetch(
-      `/api/makeTransaction?${searchParams.toString()}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      }
-    )
+    const response = await fetch(`/api/makeTransaction?${searchParams.toString()}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
 
-    const json = (await response.json()) as MakeTransactionOutputData
+    const json = await response.json() as MakeTransactionOutputData
 
     if (response.status !== 200) {
-      console.error(json)
-      return
+      console.error(json);
+      return;
     }
 
     // Deserialize the transaction from the response
-    const transaction = Transaction.from(
-      Buffer.from(json.transaction, 'base64')
-    )
-    setTransaction(transaction)
-    setMessage(json.message)
-    console.log(transaction)
+    const transaction = Transaction.from(Buffer.from(json.transaction, 'base64'));
+    setTransaction(transaction);
+    setMessage(json.message);
+    console.log(transaction);
   }
 
   useEffect(() => {
@@ -129,7 +76,7 @@ export default function Checkout() {
   // Send the fetched transaction to the connected wallet
   async function trySendTransaction() {
     if (!transaction) {
-      return
+      return;
     }
     try {
       await sendTransaction(transaction, connection)
@@ -148,15 +95,12 @@ export default function Checkout() {
     const interval = setInterval(async () => {
       try {
         // Check if there is any transaction for the reference
-        const signatureInfo = await findTransactionSignature(
-          connection,
-          reference
-        )
-        router.push('/confirmed')
+        const signatureInfo = await findTransactionSignature(connection, reference)
+        console.log('They paid!!!')
       } catch (e) {
         if (e instanceof FindTransactionSignatureError) {
           // No transaction found yet, ignore this error
-          return
+          return;
         }
         console.error('Unknown error', e)
       }
@@ -168,11 +112,9 @@ export default function Checkout() {
 
   if (!publicKey) {
     return (
-      <div className="flex flex-col items-center gap-8">
-        <div>
-          <BackLink href="/buy">Cancel</BackLink>
-        </div>
-        <div ref={qrRef} />
+      <div className='flex flex-col gap-8 items-center'>
+        <div><BackLink href='/buy'>Cancel</BackLink></div>
+
         <WalletMultiButton />
 
         <p>You need to connect your wallet to make transactions</p>
@@ -181,23 +123,15 @@ export default function Checkout() {
   }
 
   return (
-    <div className="flex flex-col items-center gap-8">
-      <div>
-        <BackLink href="/buy">Cancel</BackLink>
-      </div>
+    <div className='flex flex-col gap-8 items-center'>
+      <div><BackLink href='/buy'>Cancel</BackLink></div>
 
       <WalletMultiButton />
 
-      {message ? (
-        <p>
-          {message} Please approve the transaction using your wallet{' '}
-          <div ref={qrRef} />
-        </p>
-      ) : (
-        <p>
-          Creating transaction... <Loading />
-        </p>
-      )}
+      {message ?
+        <p>{message} Please approve the transaction using your wallet</p> :
+        <p>Creating transaction... <Loading /></p>
+      }
     </div>
   )
 }
